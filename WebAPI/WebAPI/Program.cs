00fb377c80bbef1ea0c3dc0e0a1b3ac164234121
Roger <morenoroger001@gmail.com>
2024-12-26 12:01:00 -0500

    feat: Add Swagger
    remove unnecessary files in /obj and /bin

--- WebAPI/WebAPI/Program.cs
+++ WebAPI/WebAPI/Program.cs
@@ -10,6 +10,22 @@ builder.Services.AddApplicationServices();
 
 builder.Services.AddControllers();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "WebAPI Project",
+        Version = "v1",
+        Description = "API for managing and interacting with the database in plantsdb",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Name = "Your Name",
+            Email = "youremail@example.com"
+        }
+    });
+});
+
+
 var app = builder.Build();
 
 app.MapControllers();
@@ -18,6 +34,13 @@ app.MapControllers();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI Project v1");
+        c.RoutePrefix = string.Empty; // Para que esté disponible en la raíz '/'
+    });
 }
 
 app.UseHttpsRedirection();
