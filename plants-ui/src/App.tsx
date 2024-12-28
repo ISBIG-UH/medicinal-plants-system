ef175cf51297c23d412c525b4a1cb31bf76651d6
Roger <morenoroger001@gmail.com>
2024-12-28 13:35:39 -0500

    feat: Add basic Footer

--- plants-ui/src/App.tsx
+++ plants-ui/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
 import NavMenu from './components/NavMenu';
+import Footer from './components/Footer';
 
 
 const routes: RouteItem[] = [
@@ -14,17 +15,19 @@ const navItems: NavItem[] = routes.map(route => ({ label: route.label, href: rou
 
 function App() {
   return (
-    <>
+    <div className='flex flex-col min-h-screen'>
       <NavMenu navItems={navItems} />
-      <Routes>
-        {
-          routes.map((route) => (
-            <Route key={route.label} path={route.href} element={route.component} />
-          ))
-        }
-      </Routes>
-      
-    </>
+      <div className='flex-grow bg-background'>
+        <Routes>
+          {
+            routes.map((route) => (
+              <Route key={route.label} path={route.href} element={route.component} />
+            ))
+          }
+        </Routes>
+      </div>
+      <Footer />
+    </div>
   )
 }
 
---
+++ plants-ui/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+  return (
+    <footer className="bg-gray-300 px-4 py-6 shadow-background shadow-inner">
+      {/* <img src="/uhlogo_b.svg" alt="Logo" className="mx-auto h-16 lg:h-16 bg-slate-400" /> */}
+      <p className="text-center">Hecho con ❤️</p>
+    </footer>
+  );
+}
+
+export default Footer;
