15655d971bce50955d5837259178c7e699b1f8e0
Roger <morenoroger001@gmail.com>
2024-12-29 00:56:16 -0500

    style: Name site as BotaniQ

--- plants-ui/src/components/NavMenu.tsx
+++ plants-ui/src/components/NavMenu.tsx
@@ -13,9 +13,9 @@ function NavMenu({navItems}: Props) {
   return (
     <nav className="bg-primary lg:flex lg:justify-between lg:items-center lg:px-32">
       <div className="flex items-center justify-between px-4 pt-4 pb-2 lg:pt-2">
-        <Link className="text-white font-bold text-4xl flex" to="/">
+        <Link className="text-white font-bold text-4xl flex items-center" to="/">
           <img className="max-h-10 lg:max-h-12" src="1.png" />
-          {/* <img className="max-h-14" src="jbn.svg" /> */}
+          <img className="max-h-10 ml-1" src="BotaniQ_bg_secondary.png" />
         </Link>
         <button
           className="text-secondary lg:hidden"
