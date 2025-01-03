fbffca59cbe7341877fb5ffa2f234a76df7ab2db
Roger <morenoroger001@gmail.com>
2025-01-03 16:07:02 -0500

    feat: Add monograph addition template as sidebar item

---
+++ plants-admin-ui/src/components/AddMonographBoard.tsx
@@ -0,0 +1,9 @@
+
+
+function AddMonographBoard() {
+    return(
+        <h2>Add monograph board</h2>
+    );
+}
+
+export default AddMonographBoard;
--- plants-admin-ui/src/components/Home.tsx
+++ plants-admin-ui/src/components/Home.tsx
@@ -1,13 +1,16 @@
 import { PiPlantBold } from "react-icons/pi";
 import { TbListLetters } from "react-icons/tb";
 import { IoIosSearch } from "react-icons/io";
+import { RiAddCircleLine } from "react-icons/ri";
 import NavMenu from "./NavMenu";
 import SideBar from "./SideBar";
 import ContextSearchBoard from "./ContextSearchBoard";
 import IndexSearchBoard from "./IndexSearchBoard";
+import AddMonographBoard from "./AddMonographBoard";
 
 const categories = [
   { icon: <PiPlantBold />, items: [
+    { label: "Nueva monografía", icon: <RiAddCircleLine size={24} />, element: <AddMonographBoard />},
     { label: "Búsqueda por índice", icon: <TbListLetters size={24} />, element: <IndexSearchBoard />},
     { label: "Búsqueda contextual", icon: <IoIosSearch size={24} />, element: <ContextSearchBoard />},
   ]}
