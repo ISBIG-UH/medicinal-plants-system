1112521b8998aae39dc781ebba1960e0b851fb9b
cav-ipb <vcardent@ipb-halle.de>
2025-04-18 03:38:02 -0400

    chore: add text search result placeholder test

---
+++ BQ.Client/src/features/text-search/components/__tests__/text-search-result-placehoder.test.tsx
@@ -0,0 +1,16 @@
+import { describe, expect, it } from 'vitest';
+import { getByRole, render, screen } from '@testing-library/react';
+import SearchResult from '../text-search-result';
+import { getMonographMock } from '../../../../test';
+import TextSearchResultPlaceholder from '../text-search-result-placeholder';
+
+describe('Text Search Result Placeholder', () => {
+    const page = () => <TextSearchResultPlaceholder />;
+
+    it('It should be comformed by three placeholders', async () => {
+        render(page());
+
+        const div = screen.getByTestId('text-search-result-placeholder');
+        expect(div.children).toHaveLength(3);
+    });
+});
