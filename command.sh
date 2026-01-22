b5b965327954bf3b39a074e849c23a1722577137
cav-ipb <vcardent@ipb-halle.de>
2026-01-22 10:24:17 -0500

    fix: fix nginx config files for certbot

--- command.sh
+++ command.sh
@@ -6,3 +6,25 @@ docker compose run --rm certbot certonly \
   --email niccolo.devops.floratta@gmail.com \
   --agree-tos \
   --no-eff-email
+
+
+docker compose --profile http up -d http-client
+docker compose run --rm certbot-init certonly \
+  --webroot \
+  --webroot-path=/var/www/certbot \
+  -d botaniq.cloud \
+  -d www.botaniq.cloud \
+  --email niccolo.devops.floratta@gmail.com \
+  --agree-tos \
+  --no-eff-email
+docker compose stop http-client
+
+
+docker compose run --rm certbot certonly \
+    --webroot \
+    --webroot-path=/var/www/certbot \
+    -d botaniq.cloud \
+    -d www.botaniq.cloud \
+    --email niccolo.devops.floratta@gmail.com \
+    --agree-tos \
+    --no-eff-email
